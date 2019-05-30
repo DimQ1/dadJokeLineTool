7f90bf52446a0037c21aa9ca8b013bcffd89e895
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-05-30 10:25:18 +0300

    fixed functionality of an application

---
+++ askTypeTask.js
@@ -0,0 +1,24 @@
+const prompts = require('prompts');
+
+module.exports = async () => {
+    const questions = [
+        {
+            type: 'select',
+            name: 'value',
+            message: 'select type query?',
+            choices: [
+                { title: 'Random', value: 0 },
+                { title: 'Search Tearm', value: 1 },
+                { title: 'Display liderboard joke', value: 2 }
+            ]
+        },
+        {
+            type: prev => prev === 1 ? 'text' : null,
+            name: 'searchTerm',
+            message: 'input search term please'
+        }
+    ];
+    const result = await prompts(questions);
+
+    return { value: result.value, searchTerm: result.searchTerm };
+};
